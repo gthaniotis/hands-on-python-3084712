afa1cc641d1f51e64ea8d24ecfba40ea0a1f11ef
Georgios Thaniotis <gthaniotis@gmail.com>
2025-01-20 16:51:22 +0000

    TEST

--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -7,12 +7,12 @@ CODE_SPACE = "code_space"
 LOCAL = "local"
 
 current_env = os.environ.get("ENV_NAME", DEVELOPMENT)
-
+print(current_env)
 if current_env == DEVELOPMENT:
     print("Development environment")
 elif current_env == PRODUCTION:
     print("Production environment")
-elif current_env == STAGING:
+elif current_env.lower() == STAGING:
     print("Staging environment")
 elif current_env in [CODE_SPACE, LOCAL]:
     print("Codespace or local environment")
